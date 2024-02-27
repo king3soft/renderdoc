eb3af97b741cdcb26378500d8457a72da2218986
w_lixiaofeng <w_lixiaofeng@kingsoft.com>
2024-02-27 15:18:57 +0800

    update action

---
+++ buildAndroid.bat
@@ -0,0 +1,37 @@
+@echo off
+set BUILD_DIR=build-android
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+cmake -DBUILD_ANDROID=On -DANDROID_ABI=armeabi-v7a -GNinja ..
+if exist lib rd /s /q lib
+ninja
+cd ..
+copy build-android\\bin\\org.renderdoc.renderdoccmd.arm32.apk .\\*\\Development\\plugins\\android\\org.renderdoc.renderdoccmd.arm32.apk
+
+
+set BUILD_DIR=build-android64
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+cmake -DBUILD_ANDROID=On -DANDROID_ABI=arm64-v8a -GNinja ..
+if exist lib rd /s /q lib
+ninja
+cd ..
+copy build-android64\\bin\\org.renderdoc.renderdoccmd.arm64.apk .\\*\\Development\\plugins\\android\\org.renderdoc.renderdoccmd.arm64.apk
+
+
+del /s *.vc.db
+del /s *.tlog
+del /s *.log
+del /s *.idb
+del /s *.pdb
+del /s *.lastbuildstate
+del /s *.ilk
+del /s *.exp
+del /s *.obj
+del /s *.iobj
+del /s *.ipch
+del /s *.tlh
+del /s *.obj.enc
+del /s *.tli
+del /s *.exp
+del /s *.pch
